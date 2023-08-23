a7d6eaf429f171952524a3c04dbda20e4c51475c
Christian Monti <cpmonti90@gmail.com>
2023-08-22 23:09:52 -0700

    updated table class and aero tables

--- SW/Common/Tables/Table1D.h
+++ SW/Common/Tables/Table1D.h
@@ -5,8 +5,10 @@ template <typename T>
 class Table1D
 {
 public:
-    Table1D(const T *data) : m_data(data),
-                             m_size(sizeof(data) / sizeof(T))
+    Table1D(const T *independentVar, const T *data)
+        : m_data(data),
+          m_independentVar(independentVar),
+          m_size(sizeof(data) / sizeof(T))
     {
     }
 
@@ -17,8 +19,28 @@ public:
         return m_data[index];
     }
 
-    virtual T lookUp(const T x) const = 0;
+    virtual T lookUp(const T x) const
+    {
+        if (x < m_independentVar[0])
+        {
+            return extrapolate(x, m_independentVar[0], m_data[0], (m_data[1] - m_data[0]) / (m_independentVar[1] - m_independentVar[0]));
+        }
+        else if (x > m_independentVar[m_size - 1u])
+        {
+            return extrapolate(x, m_independentVar[m_size - 1u], m_data[m_size - 1u], (m_data[m_size - 1u] - m_data[m_size - 2u]) / (m_independentVar[m_size - 1u] - m_independentVar[m_size - 2u]));
+        }
+        else
+        {
+            unsigned int idx = 0u;
+            while (x > m_independentVar[idx + 1u])
+            {
+                idx++;
+            }
+            return interpolate(idx, m_independentVar[idx], x, m_independentVar[idx + 1u] - m_independentVar[idx]);
+        }
+    }
 
+protected:
     T interpolate(const double idx, const T baseX, const T interpX, const T step) const
     {
         T slope = (m_data[static_cast<unsigned int>(idx) + 1u] - m_data[static_cast<unsigned int>(idx)]) / step;
@@ -30,8 +52,8 @@ public:
         return baseY + slope * (extrapX - baseX);
     }
 
-protected:
     const T *m_data;
+    const T *m_independentVar;
     const unsigned int m_size;
 };
 
--- Sim/Environment/Aero/Algorithm/Aero.cpp
+++ Sim/Environment/Aero/Algorithm/Aero.cpp
@@ -20,7 +20,7 @@ void Aero::initialize()
 
 void Aero::exec(const AeroTypes::InData& inData, AeroTypes::OutData& outData)
 {
-    // airDensity = AeroTables::densityTable.lookUp(altSeaLevel);
+    airDensity = AeroTables::densityTable.lookUp(altSeaLevel);
     dynamicPressure = 0.5 * airDensity * myMath::SQ(velT);
 
     updateCoefficients();
